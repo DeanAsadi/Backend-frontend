db9f23d0c9542ad5a0f4a055a4779aab7df85fce
Dean Asadi <eng.de86@gmail.com>
2018-12-16 19:20:47 -0600

    controller

--- server/controller/product_controller.js
+++ server/controller/product_controller.js
@@ -4,5 +4,27 @@ module.exports = {
     db.create_product()
       .then(() => res.status(200))
       .catch(err => console.log("Create Product Error ", err));
+  },
+  getAll: (req, res, next) => {
+    const db = req.app.get("db");
+    db.read_products()
+      .then(products => res.status(200).send(products))
+      .catch(err => console.log("READ ALL Product Error ", err));
+  },
+  getOne: (req, res, next) => {
+    const db = req.app.get("db");
+    db.read_product()
+      .then(product => res.status(200).send(product))
+      .catch(err => console.log("READ ONE Product Error ", err));
+  },
+  update: (req, res, next) => {
+    const db = req.app.get("db");
+    db.update_product()
+      .then(() => res.status(200))
+      .catch(err => console.log("UPDATE  Product Error ", err));
+  },
+  delete: (req, res, next) => {
+    const db = req.app.get("db");
+    db.delete_product;
   }
 };
