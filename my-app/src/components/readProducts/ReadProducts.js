2e9c6e0099fc2e7f37f25bee5efbc950a71ca21d
Dean Asadi <eng.de86@gmail.com>
2018-12-17 02:18:35 -0600

    updating

--- my-app/src/components/readProducts/ReadProducts.js
+++ my-app/src/components/readProducts/ReadProducts.js
@@ -1,14 +1,43 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class ReadProducts extends Component {
   constructor() {
     super();
     this.state = {
-      product: ""
+      products: [],
+      isLoading: false
     };
   }
+  componentDidMount() {
+    axios
+      .get("http://localhost:4000/api/products")
+      .then(res => {
+        this.setState({ products: res.data });
+      })
+      .catch(err => console.log("ERROR---->", err));
+  }
+
   render() {
-    return <div>ReadProducts</div>;
+    const { products, isLoading } = this.state;
+
+    const displayProducts = isLoading ? (
+      <p>Loading...</p>
+    ) : (
+      products.map((elm, ind) => {
+        return (
+          <div key={ind}>
+            {elm.name}
+            <br />
+            {elm.description} <br />
+            {elm.price} <br />
+            {elm.image_url} <br />
+          </div>
+        );
+      })
+    );
+
+    return <div>{displayProducts}</div>;
   }
 }
 export default ReadProducts;
