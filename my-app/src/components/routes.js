22f18b14607458433c511cc2da35af8a548cf88d
Dean Asadi <eng.de86@gmail.com>
2018-12-17 02:19:33 -0600

    updating routes

--- my-app/src/components/routes.js
+++ my-app/src/components/routes.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import ReadProducts from "./readProducts/ReadProducts";
-import DeleteProduct from "./deleteProduct/DeleteProduct";
 import CreateProduct from "./createProduct/CreateProduct";
 
 export default (
   <Switch>
     <Route exact component={ReadProducts} path="/home" />
-    <Route component={DeleteProduct} path="/delete" />
     <Route component={CreateProduct} path="/newProduct" />
   </Switch>
 );
