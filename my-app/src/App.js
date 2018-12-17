c4120cfd383eb883c8e621983a422f4b683a75e9
Dean Asadi <eng.de86@gmail.com>
2018-12-17 00:03:41 -0600

    creating react components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,9 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">React...</header>
-      </div>
-    );
+    return <div className="App">React...</div>;
   }
 }
 
