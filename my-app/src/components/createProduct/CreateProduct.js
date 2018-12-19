839a2c65ed730f920a6fd8e0c6b760d795a60b83
Dean Asadi <eng.de86@gmail.com>
2018-12-19 11:26:44 -0600

    creating product

--- my-app/src/components/createProduct/CreateProduct.js
+++ my-app/src/components/createProduct/CreateProduct.js
@@ -8,20 +8,53 @@ class CreateProduct extends Component {
       product: "",
       description: "",
       imageUrl: "",
-      price: ""
+      price: 0
     };
   }
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
   onSubmitHandeler = e => {
     e.preventDefault();
     const { product, description, imageUrl, price } = this.state;
     if ((product, description, imageUrl, price)) {
-      axios.post("http://localhost:4000/api/product");
+      axios.post("http://localhost:4000/api/product", {
+        product,
+        description,
+        imageUrl,
+        price
+      });
     }
   };
 
   render() {
-    return <div>CreateProduct</div>;
+    const { product, description, imageUrl, price } = this.state;
+    return (
+      <div>
+        <form onClick={this.onSubmitHandeler}>
+          <input
+            name="product"
+            value={product}
+            onChange={this.handleChange}
+            placeholder="Product"
+          />
+          <input
+            name="imageUrl"
+            value={imageUrl}
+            onChange={this.handleChange}
+            placeholder=" Image"
+          />
+          <input
+            name="price"
+            vallue={price}
+            onChange={this.handleChange}
+            placeholder="Price "
+          />
+          <button>Add Product</button>
+        </form>
+      </div>
+    );
   }
 }
 export default CreateProduct;
