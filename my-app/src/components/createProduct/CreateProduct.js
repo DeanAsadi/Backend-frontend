725f86a42c1da671d2bdb22616257c402becebcc
Dean Asadi <eng.de86@gmail.com>
2018-12-19 11:18:37 -0600

    axios

--- my-app/src/components/createProduct/CreateProduct.js
+++ my-app/src/components/createProduct/CreateProduct.js
@@ -5,11 +5,20 @@ class CreateProduct extends Component {
   constructor() {
     super();
     this.state = {
-      product: []
+      product: "",
+      description: "",
+      imageUrl: "",
+      price: ""
     };
   }
 
-  createProduct = () => {};
+  onSubmitHandeler = e => {
+    e.preventDefault();
+    const { product, description, imageUrl, price } = this.state;
+    if ((product, description, imageUrl, price)) {
+      axios.post("http://localhost:4000/api/product");
+    }
+  };
 
   render() {
     return <div>CreateProduct</div>;
