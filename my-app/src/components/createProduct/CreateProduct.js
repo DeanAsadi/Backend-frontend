e85c6e54af32da4d9298b07bdda39da50a1dd144
Dean Asadi <eng.de86@gmail.com>
2018-12-23 23:42:27 -0600

    starting all over

--- my-app/src/components/createProduct/CreateProduct.js
+++ my-app/src/components/createProduct/CreateProduct.js
@@ -4,67 +4,14 @@ import axios from "axios";
 class CreateProduct extends Component {
   constructor() {
     super();
-    this.state = {
-      name: "",
-      description: "",
-      image_url: "",
-      price: 0
-    };
+    this.state = {};
   }
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
-  onSubmitHandeler = e => {
-    e.preventDefault();
-    const { name, description, image_url, price } = this.state;
-    if ((name, description, image_url, price)) {
-      axios
-        .post("/api/product", {
-          name,
-          description,
-          image_url,
-          price
-        })
-        .then(() => {
-          this.props.history.replace("/home");
-        });
-    }
-  };
-
   render() {
-    const { name, description, image_url, price } = this.state;
-    return (
-      <div>
-        <form onClick={this.onSubmitHandeler}>
-          <input
-            name="name"
-            value={name}
-            onChange={this.handleChange}
-            placeholder="name"
-          />
-          <input
-            name="image_url"
-            value={image_url}
-            onChange={this.handleChange}
-            placeholder=" Image"
-          />
-          <input
-            name="price"
-            vallue={price}
-            onChange={this.handleChange}
-            placeholder="Price "
-          />
-          <input
-            name="description"
-            vallue={description}
-            onChange={this.handleChange}
-            placeholder="description "
-          />
-          <button>Add Product</button>
-        </form>
-      </div>
-    );
+    return <div />;
   }
 }
 export default CreateProduct;
--- my-app/src/components/readProducts/ReadProducts.js
+++ my-app/src/components/readProducts/ReadProducts.js
@@ -2,53 +2,20 @@ import React, { Component } from "react";
 import axios from "axios";
 
 import "./readProducts.css";
+import CreateProduct from "../createProduct/CreateProduct";
 
 class ReadProducts extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      products: [],
-      isLoading: false
-    };
-  }
-  componentDidMount() {
-    axios
-      .get("http://localhost:4000/api/products")
-      .then(response => {
-        this.setState({ products: response.data });
-      })
-      .catch(err => console.log("ERROR---->", err));
+    this.state = {};
   }
 
-  deleteProductHandeler = id => {
-    axios.delete(`http://localhost:4000/api/product/${id}`).then(() => {
-      window.location.reload(); // To reload page after delete
-    });
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
   };
 
   render() {
-    const { products, isLoading } = this.state;
-    const displayProducts = isLoading ? (
-      <p>Loading...</p>
-    ) : (
-      products.map((elm, ind) => {
-        console.log("elm--->", elm);
-        return (
-          <div key={elm.product_id}>
-            {elm.name}
-            <br />
-            {elm.description} <br />
-            {elm.price} <br />
-            {elm.image_url} <br />
-            <button onClick={id => this.deleteProductHandeler(elm.product_id)}>
-              Delete
-            </button>
-          </div>
-        );
-      })
-    );
-
-    return <div className="displayProducts">{displayProducts}</div>;
+    return <div className="displayProducts" />;
   }
 }
 export default ReadProducts;
